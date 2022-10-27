c3535fbe7548bf43d5d152b527fc6209dcb7f552
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2022-10-27 10:54:59 -0500

    Castro

---
+++ src/main/java/com/mycompany/finca/Finca.java
@@ -0,0 +1,24 @@
+/*
+	 * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+	 * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+	 */
+	package com.mycompany.finca;
+	
+
+	/**
+	 *
+	 * @author Araceli
+	 */
+	public abstract class Finca {
+	    private String TipoDeTierra;
+	    private int antiguedad;
+	    private String Condiciones;
+	    private int tiempoDeSiembra;
+	    
+	    public void tipoDeTierra( int antiguedad, String condiciones){}
+	    public void destino( int tiempoDeCosecha){}
+	    public void tipoDeSiembra(){}
+	
+
+	}
+
---
+++ src/main/java/com/mycompany/finca/FincaAgricola.java
@@ -0,0 +1,21 @@
+/*
+	 * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+	 * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+	 */
+	
+
+	package com.mycompany.finca;
+	
+
+	/**
+	 *
+	 * @author Araceli
+	 */
+	public class FincaAgricola {
+	
+
+	    public static void main(String[] args) {
+	        System.out.println("Hello World!");
+	    }
+	}
+
